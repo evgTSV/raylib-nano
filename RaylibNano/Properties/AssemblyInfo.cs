d5fecc4a2ee5c39f7869ceb93bc77c006d06e84b
evgTSV <freefiery@gmail.com>
2025-01-04 13:54:15 +0500

    Добавьте файлы проекта.

---
+++ RaylibNano/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Raylib library based-on nanoFramework for programming videogames on embedded devices")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
